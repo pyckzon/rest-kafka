dd37e3938c9e30ec2e4e52244aa6ffb9f2cc92bd
pyckzon <developer.csalazar@gmail.com>
2019-03-25 18:41:15 -0600

    Add files via upload

---
+++ SenderDual/src/main/java/com/csalazar/app/SenderDual/SenderDualApplication.java
@@ -0,0 +1,13 @@
+package com.csalazar.app.SenderDual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderDualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderDualApplication.class, args);
+	}
+
+}
---
+++ SenderDual/src/test/java/com/csalazar/app/SenderDual/SenderDualApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csalazar.app.SenderDual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SenderDualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
