193b17f786a508c049f38a9e4fc546bf3f2bbeaf
pyckzon <developer.csalazar@gmail.com>
2019-03-25 18:42:00 -0600

    Add files via upload

---
+++ ListenerDual/src/main/java/com/csalazar/app/ListenerDual/ListenerDualApplication.java
@@ -0,0 +1,13 @@
+package com.csalazar.app.ListenerDual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerDualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenerDualApplication.class, args);
+	}
+
+}
