78632e51ee804fe9a18ef6ec313906342d0199b8
pyckzon <developer.csalazar@gmail.com>
2019-03-25 21:10:56 -0600

    Add files via upload

---
+++ Receptor/src/main/java/com/csalazar/app/Receptor/ReceptorApplication.java
@@ -0,0 +1,13 @@
+package com.csalazar.app.Receptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptorApplication.class, args);
+	}
+
+}
